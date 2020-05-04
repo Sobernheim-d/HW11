d295d504a5aa1f04a6b09fec0b0fd590c4e71b1b
Steve Jobs <dwsobernheim@gmail.com>
2020-05-04 01:28:07 -0600

    added additional photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,17 +9,6 @@ const mysql = require("mysql");
 
 // Connect to MySQL
 const pool = mysql.createPool({
-    "connectionLimit"   : 10,
-    "host"              : "ysp9sse09kl0tzxj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    "port"              : 3306,
-    "user"              : "l7g2iikw6shnok4f",
-    "password"          : "atw1sx49swurflkj",
-    "database"          : "yfpdqkylr03ge45y",
-    "multipleStatements": true
-});
-
-/* For testing on localhost
-const connection = mysql.createConnection({
     "host"              : "localhost",
     "port"              : 3306,
     "user"              : "root",
@@ -27,12 +16,10 @@ const connection = mysql.createConnection({
     "database"          : "friend_finder_db",
     "multipleStatements": true
 });
-connection.connect(error => {
-    if (error) throw error;
-    console.log(`Database connected as thread ${connection.threadId}.`);
-});
-*/
 
+// connection.connect(error => {    if (error) throw error;
+//     console.log(`Database connected as thread ${connection.threadId}.`);
+// });
 
 
 /****************************************************************************
